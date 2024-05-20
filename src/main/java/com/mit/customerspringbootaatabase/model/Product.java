aafd1f9df1f154a1260dff5898edfc5afa90c747
javabyram <bandalemp0208@gmail.com>
2024-05-20 15:53:01 +0530

    xyz

--- src/main/java/com/mit/customerspringbootaatabase/model/Product.java
+++ src/main/java/com/mit/customerspringbootaatabase/model/Product.java
@@ -1,8 +1,21 @@
+package com.mit.customerspringbootaatabase.model;
 
-public class Product{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
-  private Integer id;
-  private String name;
-  private String description;
-  
+@Data
+@Entity
+@Table
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String description;
+
 }
